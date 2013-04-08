194642d0a29577fb0345a80a2764c1061a748cad
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-04-07 20:25:54 -0400

    Cleanup supplier welcome email.

--- app/mailers/spree/supplier_mailer.rb
+++ app/mailers/spree/supplier_mailer.rb
@@ -1,10 +1,10 @@
 class Spree::SupplierMailer < ActionMailer::Base
 
-  default :from => Spree::MailMethod.current.try(:preferred_mails_from) || 'replace@test.com'
+  default :from => Spree::MailMethod.current.try(:preferred_mails_from) || 'replace@example.com'
 
   def welcome(supplier)
     @supplier = supplier
-    mail :to => @supplier.email_with_name, :subject => I18n.t('spree_drop_ship.supplier_mailer.welcome.subject')
+    mail :to => @supplier.email_with_name, :subject => I18n.t('spree.supplier_mailer.welcome.subject')
   end
 
 end
--- spec/features/admin/orders_spec.rb
+++ spec/features/admin/orders_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'Admin - Orders', js: true do
 
   it 'Supplier should not be authorized' do
+    create(:user) # create extra user so admin role isnt assigned to the user we login as
     login_user create(:supplier_user)
     visit spree.admin_orders_path
     page.should have_content('Unauthorized')
