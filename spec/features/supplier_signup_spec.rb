09645702e95123562b1f64ac60d3d9b5b4d26ec6
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-04-26 18:52:56 -0400

    Fix spec now that js form validation is working.

--- spec/features/supplier_signup_spec.rb
+++ spec/features/supplier_signup_spec.rb
@@ -84,7 +84,7 @@ feature 'Supplier Signup', js: true do
         select 'Vermont', from: 'supplier[address_attributes][state_id]'
         fill_in 'supplier[address_attributes][phone]', with: '555-555-5555'
         click_button 'Sign Up'
-        page.should have_content('Address is invalid')
+        page.should have_content('This field is required.')
       end
 
     end
