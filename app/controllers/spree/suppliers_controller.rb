5676e0409354dc900e06907bf1157879e9c13a89
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-05-07 16:14:06 -0400

    Remove authenticated check.

--- app/controllers/spree/suppliers_controller.rb
+++ app/controllers/spree/suppliers_controller.rb
@@ -1,6 +1,5 @@
 class Spree::SuppliersController < Spree::StoreController
 
-  before_filter :check_authentication
   before_filter :check_if_supplier, only: [:create, :new]
   ssl_required
 
@@ -22,13 +21,6 @@ class Spree::SuppliersController < Spree::StoreController
   end
 
   private
-
-  # Check user is logged in.
-  def check_authentication
-    return if spree_current_user
-    flash[:error] = I18n.t(:must_be_logged_in)
-    redirect_to spree_login_path
-  end
 
   def check_if_supplier
     if spree_current_user.supplier?
