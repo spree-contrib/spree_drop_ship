ec2e2fb6ec0c8a847de123d40d973def829d97ee
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-10-01 23:07:20 -0400

    Fix stock location decorator spec & durably_decorate the method.

--- app/models/spree/stock_location_decorator.rb
+++ app/models/spree/stock_location_decorator.rb
@@ -6,11 +6,13 @@ Spree::StockLocation.class_eval do
   scope :with_supplier, ->(supplier) { where(supplier_id: supplier) }
 
   # Wrapper for creating a new stock item respecting the backorderable config and supplier
-  def propagate_variant(variant)
+  durably_decorate :propagate_variant, mode: 'soft', sha: 'f35b0d8a811311d4886d53024a9aa34e3aa5f8cb' do |variant|
     if self.supplier_id.nil? or self.supplier_id == variant.product.supplier_id
       self.stock_items.create!(variant: variant, backorderable: self.backorderable_default)
     else
       # Never allow alternate suppliers locations to take backorders for a product.
+      # TODO: consider removing creating a stock item at all... issue could be multiple sellers wanting to sell the same product,
+      #       but if we solve multiple supplier problem by just creating a variant for each supplier we're all set to remove.
       self.stock_items.create!(variant: variant, backorderable: false)
     end
   end
--- spec/models/spree/stock_location_decorator_spec.rb
+++ spec/models/spree/stock_location_decorator_spec.rb
@@ -4,35 +4,30 @@ describe Spree::StockLocation do
 
   it { should belong_to(:supplier) }
 
-  subject { create(:stock_location_with_items, backorderable_default: true) }
-  let(:variant) { subject.stock_items.order(:id).first.variant }
+  subject { create(:stock_location, backorderable_default: true) }
 
   context "propagate variants" do
+
+    let(:variant) { build(:variant) }
     let(:stock_item) { subject.propagate_variant(variant) }
-  
-    it "creates a new stock item" do
-      expect {
-        subject.propagate_variant(variant)
-      }.to change{ Spree::StockItem.count }.by(1)
-    end
-  
+
     context "passes backorderable default config" do
       context "true" do
         before { subject.backorderable_default = true }
         it { stock_item.backorderable.should be_true }
       end
-    
+
       context "false" do
         before { subject.backorderable_default = false }
         it { stock_item.backorderable.should be_false }
       end
     end
-  
+
     context 'sets to false for non supplier variants' do
       before { subject.supplier_id = create(:supplier).id }
       it { stock_item.backorderable.should be_false }
     end
-  
+
   end
 
 end
