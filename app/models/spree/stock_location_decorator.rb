4a9796ba1cea2d8760cf42ca979928ee3c99c73a
Jeff Squires <jeff.squires@gmail.com>
2013-10-01 22:13:16 -0400

    fixes stack level too deep error for propagate_variant

--- app/models/spree/stock_location_decorator.rb
+++ app/models/spree/stock_location_decorator.rb
@@ -6,20 +6,13 @@ Spree::StockLocation.class_eval do
   scope :with_supplier, ->(supplier) { where(supplier_id: supplier) }
 
   # Wrapper for creating a new stock item respecting the backorderable config and supplier
-  # def propagate_variant(variant)
-  #   self.stock_items.create!(variant: variant, backorderable: self.backorderable_default)
-  #   if self.supplier_id.nil? or self.supplier_id == variant.product.supplier_id
-  #     # puts 'yay'
-  #     # item = self.stock_items.build(variant: variant, backorderable: self.backorderable_default)
-  #     # puts item.inspect
-  #     # item.save
-  #     # puts 'after create'
-  #     self.stock_items.create!(variant: variant, backorderable: self.backorderable_default)
-  #   else
-  #     puts 'else'
-  #     # Never allow alternate suppliers locations to take backorders for a product.
-  #     self.stock_items.create!(variant: variant, backorderable: false)
-  #   end
-  # end
+  def propagate_variant(variant)
+    if self.supplier_id.nil? or self.supplier_id == variant.product.supplier_id
+      self.stock_items.create!(variant: variant, backorderable: self.backorderable_default)
+    else
+      # Never allow alternate suppliers locations to take backorders for a product.
+      self.stock_items.create!(variant: variant, backorderable: false)
+    end
+  end
 
 end
