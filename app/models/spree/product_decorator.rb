1107d1094c91c040d36284d168f4388f9d646c45
matt dundas <mdundas@gmail.com>
2013-10-31 13:49:51 -0400

    removed attr_accessible

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,8 +2,6 @@ Spree::Product.class_eval do
 
   belongs_to :supplier
   attr_accessible :supplier_id
-
-  attr_accessible :name, :permalink, :description, :price, :cost_price, :cost_currency, :available_on, :shipping_category_id, :tax_category_id, :taxon_ids, :option_type_ids, :meta_keywords, :meta_description
   
   # Returns true if the product has a drop shipping supplier.
   def supplier?
