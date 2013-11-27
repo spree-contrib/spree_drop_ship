fed842cb8d3a3ae62cbd5cf932f351b5bfef15fc
matt dundas <matt@katori.com>
2013-11-27 11:19:53 -0500

    remove unnecessary whitespace

--- app/controllers/spree/suppliers_controller.rb
+++ app/controllers/spree/suppliers_controller.rb
@@ -43,6 +43,6 @@ class Spree::SuppliersController < Spree::StoreController
       flash[:error] = Spree.t('supplier_registration.already_signed_up')
       redirect_to spree.account_path and return
     end
-  end  
+  end
 
 end
