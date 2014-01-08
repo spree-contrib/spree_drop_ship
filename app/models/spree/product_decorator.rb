4efdd10ccf278dc2d62cb2a646fb0df6c0cd2a38
Jeff Dutil <jdutil@burlingtonwebapps.com>
2014-01-08 15:42:12 -0500

    Touch supplier when products updated.

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Product.class_eval do
 
-  belongs_to :supplier
+  belongs_to :supplier, touch: true
   attr_accessible :supplier_id
 
   # Returns true if the product has a drop shipping supplier.
