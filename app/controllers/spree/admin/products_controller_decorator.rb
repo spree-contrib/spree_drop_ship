445722a8e8e09073cfa180ae357a295ff65e570f
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-07-20 16:23:04 -0400

    Remove setting supplier which is done by marketplace now.

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -2,19 +2,11 @@ Spree::Admin::ProductsController.class_eval do
 
   before_filter :get_suppliers, only: [:edit, :update]
   before_filter :supplier_collection, only: [:index]
-  create.after :set_supplier
 
   private
 
   def get_suppliers
     @suppliers = Spree::Supplier.order(:name)
-  end
-
-  def set_supplier
-    if try_spree_current_user.supplier?
-      @object.master.supplier = spree_current_user.supplier
-      @object.master.save
-    end
   end
 
   # Scopes the collection to the Supplier.
