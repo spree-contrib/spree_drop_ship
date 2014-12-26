57a0bea5c527c0f040cc17e1b28dbdbc1ac26ee0
Jose Alves <j.alves@runtime-revolution.com>
2014-12-26 17:53:56 +0000

    When a Supplier adds a Product, they should be associated

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -3,6 +3,8 @@ Spree::Admin::ProductsController.class_eval do
   before_filter :get_suppliers, only: [:edit, :update]
   before_filter :supplier_collection, only: [:index]
 
+  create.after :add_product_to_supplier
+
   private
 
   def get_suppliers
@@ -13,6 +15,13 @@ Spree::Admin::ProductsController.class_eval do
   def supplier_collection
     if try_spree_current_user && !try_spree_current_user.admin? && try_spree_current_user.supplier?
       @collection = @collection.joins(:suppliers).where('spree_suppliers.id = ?', try_spree_current_user.supplier_id)
+    end
+  end
+
+  # Newly added products by a Supplier are associated with it.
+  def add_product_to_supplier
+    if try_spree_current_user && try_spree_current_user.supplier?
+      @product.add_supplier!(try_spree_current_user.supplier_id)
     end
   end
 
