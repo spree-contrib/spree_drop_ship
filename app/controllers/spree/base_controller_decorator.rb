51c239b21c535b300495ddd2e466f06d7dd61897
Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>
2016-03-21 01:39:00 +0000

    Update base_controller_decorator.rb

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -1,9 +1,10 @@
 Spree::BaseController.class_eval do
 
+  prepend_before_filter :redirect_supplier
   before_action :authorize_supplier
-
-  protected
-
+  
+  private
+  
   def authorize_supplier
     if respond_to?(:model_class, true) && model_class
       record = model_class
@@ -14,4 +15,9 @@ Spree::BaseController.class_eval do
     authorize! action, record
   end
 
+  def redirect_supplier
+    if ['/admin', '/admin/authorization_failure'].include?(request.path) && try_spree_current_user.try(:supplier)
+      redirect_to '/admin/shipments' and return false
+    end
+  end
 end
